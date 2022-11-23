530a51bd2b126a375a3097563741f77c7e52a632
Eyupcan Cakir <eypcnckr@gmail.com>
2022-11-23 23:21:11 +0300

    Adapt bootstrap template to the hockey project

---
+++ project/static/js/page.js
@@ -0,0 +1,19 @@
+function choicePage(page) {
+    const background = document.getElementsByClassName("s002")[0];
+    const title = document.getElementsByTagName("LEGEND")[0];
+    switch (page) {
+        case "teams":
+            background.style.background = "url(../static/team.webp.png)"
+            title.innerHTML = "SEARCH TEAM";
+            break;
+        case "players":
+            background.style.background = "url(../static/player.jpg)"
+            title.innerHTML = "SEARCH PLAYER";
+            break;
+        case "coaches":
+            background.style.background = "url(../static/search_img.png)"
+            title.innerHTML = "SEARCH COACH";
+            break;
+        default:
+            background.style.background = "url(../static/team.webp.png)"    }
+}
