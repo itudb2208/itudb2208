19c60ac8f18c33306b05b304dde9117d28c57309
Enceladus-py <dalsunaberat@gmail.com>
2022-12-15 10:45:02 +0300

    added login page and updated Users table

--- project/__init__.py
+++ project/__init__.py
@@ -1,9 +1,31 @@
 import os
 import importlib
 from flask import Flask
+from flask_login import LoginManager, UserMixin,AnonymousUserMixin
 from . import db
 
-BLUEPRINT_NAMES = ['home','about']  # Names of the blueprints (sub-applications) that will be registered by the main application
+BLUEPRINT_NAMES = ['home','about','login']  # Names of the blueprints (sub-applications) that will be registered by the main application
+
+# user class for login
+class User(UserMixin):
+    def __init__(self, name, password, id, active=True):
+        self.name = name
+        self.password = password
+        self.id = id
+        self.active = active
+
+    def is_active(self):
+        return self.active
+    
+    def is_authenticated(self):
+        return True
+
+# anonymous user is used for not logged users
+class Anonymous(AnonymousUserMixin):
+    name = "Anonymous"
+
+login_manager = LoginManager()
+login_manager.anonymous_user = Anonymous
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -23,7 +45,8 @@ def create_app(test_config=None):
         pass
         
     db.register_app(app)
-        
+    login_manager.init_app(app)
+
     for bp_name in BLUEPRINT_NAMES:  # Dynamic blueprint register
         bp_module = importlib.import_module(f"project.blueprints.{bp_name}")
         app.register_blueprint(bp_module.bp)
