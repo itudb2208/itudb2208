a8bc7243ff0522c0e28acab1d752ba0aee5d8b50
Eyupcan Cakir <eypcnckr@gmail.com>
2022-12-17 14:57:21 +0300

    Update routing for individual pages

---
+++ project/blueprints/player/__init__.py
@@ -0,0 +1,13 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from project.db import get_db
+
+bp = Blueprint('player', __name__, url_prefix='/search')
+
+@bp.route('/<string:tmID>', methods=['GET','POST'])
+def index(tmID):
+    # with get_db() as connection:
+    #     cursor = connection.cursor()
+    #     team = cursor.execute("SELECT * FROM Teams WHERE ")
+    return render_template('player.html',pl='yes',id=tmID)
