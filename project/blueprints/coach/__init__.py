b5244ab254a45560d8cb3a59a108ed036af6864d
Eyupcan Cakir <eypcnckr@gmail.com>
2022-12-18 00:37:34 +0300

    Minory bug fixes

--- project/blueprints/coach/__init__.py
+++ project/blueprints/coach/__init__.py
@@ -5,15 +5,15 @@ from project.db import get_db
 
 bp = Blueprint('coach', __name__, url_prefix='/search/coachID')
 
-@bp.route('/<string:tmID>', methods=['GET'])
+@bp.route('/<string:coachID>', methods=['GET'])
 def index(coachID):
     with get_db() as connection:
         cursor = connection.cursor()
         cursor.execute(f"""SELECT firstName,lastName,nameNick,height,weight,firstNHL,lastNHL,pos,birthYear,birthMon,birthDay,birthCountry,birthCity,deathYear,deathMon,deathDay,deathCountry,deathCity
-FROM Master WHERE (playerID="{coachID}")""")
+FROM Master WHERE (coachID="{coachID}")""")
         general = cursor.fetchall()
         cursor.execute(f"""SELECT Coaches.year,Coaches.lgID,Teams.name as Team_Name,Coaches.g,Coaches.w,Coaches.l,Coaches.t,postg,postw,postl,postt
-FROM Coaches JOIN Teams ON (Coaches.tmID=Teams.tmID) WHERE (coachID="{coachID}") GROUP BY Coaches.year,Teams.name")""")
+FROM Coaches JOIN Teams ON (Coaches.tmID=Teams.tmID) WHERE (coachID="{coachID}") GROUP BY Coaches.year,Teams.name""")
         stats = cursor.fetchall()
         cursor.execute(f"""SELECT award,AwardsCoaches.year
 FROM Coaches JOIN AwardsCoaches ON (AwardsCoaches.year=Coaches.year)AND(AwardsCoaches.coachID=Coaches.coachID) WHERE (Coaches.coachID="{coachID}")""")
