32f5f977a219575fcf2d47e861392807f012f939
Eyupcan Cakir <eypcnckr@gmail.com>
2022-12-19 02:39:54 +0300

    Update delete operations for individual pages

--- project/blueprints/delete/__init__.py
+++ project/blueprints/delete/__init__.py
@@ -29,3 +29,40 @@ def delete():
         cursor.fetchall()
 
     return redirect(url_for("home.index"))
+
+@bp.route('/tmID/delete', methods=['POST'])
+def teams():
+    tmID = deleteIndividual("tmID",MultiDict(request.form))
+    return redirect(url_for(f"""team.index""",tmID=tmID))
+
+@bp.route('/playerID/delete', methods=['POST'])
+def players():
+    playerID = deleteIndividual("playerID",MultiDict(request.form))
+    return redirect(url_for(f"""player.index""",playerID=playerID))
+
+@bp.route('/coachID/delete', methods=['POST'])
+def coaches():
+    coachID = deleteIndividual("coachID",MultiDict(request.form))
+    return redirect(url_for(f"""coach.index""",coachID=coachID))
+
+def deleteIndividual(which_id_type,request):
+    request.form = request # Make the dict mutable
+    years = request.form.getlist('years')
+
+    which_table = request.form.pop('which_table')
+    which_id = request.form.pop('which_id')
+
+    statement = f"""DELETE FROM {which_table} WHERE ({which_id_type} = "{which_id}") AND ("""
+
+    last = years.pop()
+
+    for year in years:
+        statement += f""" (year = "{year}") OR"""
+    statement += f""" (year = "{last}"))"""
+    
+    with get_db() as connection:
+        cursor = connection.cursor()
+        cursor.execute(statement)
+        cursor.fetchall()
+
+    return which_id
