9adc946ba34bc6e0cf182a7c28e438f49d719283
Eyupcan Cakir <eypcnckr@gmail.com>
2022-12-18 16:34:33 +0300

    Update db and individual pages

--- project/blueprints/player/__init__.py
+++ project/blueprints/player/__init__.py
@@ -9,19 +9,23 @@ bp = Blueprint('player', __name__, url_prefix='/search/playerID')
 def index(playerID):
     with get_db() as connection:
         cursor = connection.cursor()
-        cursor.execute(f"""SELECT firstName,lastName,nameNick,height,weight,firstNHL,lastNHL,pos as Position,birthYear,
+        cursor.execute(f"""SELECT firstName,lastName,nameNick,HOF.year as HallOfFame_Year,height as Player_Weight,weight as Player_Height,shootCatch,firstNHL,lastNHL,pos as Position,birthYear,
         birthMon,birthDay,birthCountry,birthCity,deathYear,deathMon,deathDay,deathCountry,deathCity
-        FROM Master WHERE (playerID=?)""", (playerID,))
+        FROM Master LEFT JOIN HOF ON (Master.hofID = HOF.hofID) WHERE (playerID=?)""", (playerID,))
         general = cursor.fetchall()
-        cursor.execute(f"""SELECT Scoring.year as Season,Scoring.lgID as League,Teams.name as Team,pos as Position,GP as Games_Played,
+        statement = f"""SELECT Scoring.year as Season,Scoring.lgID as League,Teams.name as Team,pos as Position,GP as Games_Played,
         Scoring.G as Goals,A as Assists,Scoring.Pts as Points,PIM,"+/-",PPG,PPA,SHG,SHA,GWG,GTG,SOG
-        FROM Scoring JOIN Teams ON (Scoring.tmID=Teams.tmID) WHERE (playerID=?) GROUP BY Scoring.year,Teams.name""", (playerID,))
+        FROM Scoring JOIN Teams ON (Scoring.tmID=Teams.tmID) WHERE (playerID=?) GROUP BY Scoring.year,Teams.name""", (playerID,)if general[0] == "G" else f"""SELECT Goalies.year as Season,Goalies.lgID as League,Teams.name as Team,pos as Position,GP as Games_Played,
+        Scoring.G as Goals,A as Assists,Scoring.Pts as Points,PIM,"+/-",PPG,PPA,SHG,SHA,GWG,GTG,SOG
+        FROM Scoring JOIN Teams ON (Scoring.tmID=Teams.tmID) WHERE (playerID=?) GROUP BY Scoring.year,Teams.name""", (playerID,)
+        
+        cursor.execute()
         stats = cursor.fetchall()
         cursor.execute(f"""SELECT AwardsPlayers.year as Season,award as Award
         FROM Scoring JOIN AwardsPlayers ON (AwardsPlayers.year=Scoring.year)AND(AwardsPlayers.playerID=Scoring.playerID) WHERE (Scoring.playerID=?)""", (playerID,))
         awards = cursor.fetchall()
     
-    return render_template('player.html',general_headers=["firstName","lastName","nameNick","height","weight","firstNHL","lastNHL","Position","birthYear","birthMon",
+    return render_template('player.html',general_headers=["firstName","lastName","nameNick","Player_Weight","Player_Height","shootCatch","firstNHL","lastNHL","Position","birthYear","birthMon",
     "birthDay","birthCountry","birthCity","deathYear","deathMon","deathDay","deathCountry","deathCity"],
     stats_headers=["Season","League","Team","Position","Games_Played","Goals","Assists","Points","PIM","+/-","PPG","PPA","SHG","SHA","GWG","GTG","SOG"],
     awards_headers=["Season","Award"],general=general,stats=stats,awards=awards)
