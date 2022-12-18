9c724c13e8b5527b94d532b0fe2903ba4a253e4e
sfurkan20 <fsalik25@outlook.com>
2022-12-18 14:40:23 +0300

    Replace concats with placeholders

--- project/blueprints/coach/__init__.py
+++ project/blueprints/coach/__init__.py
@@ -11,14 +11,14 @@ def index(coachID):
         cursor = connection.cursor()
         cursor.execute(f"""SELECT firstName,lastName,nameNick,height,weight,firstNHL,lastNHL,pos,birthYear,
         birthMon,birthDay,birthCountry,birthCity,deathYear,deathMon,deathDay,deathCountry,deathCity
-        FROM Master WHERE (coachID="{coachID}")""")
+        FROM Master WHERE (coachID=?)""", (coachID,))
         general = cursor.fetchall()
         cursor.execute(f"""SELECT Coaches.year as Season,Coaches.lgID as League,Teams.name as Team ,Coaches.g as Games,Coaches.w as Win,Coaches.l as Loose,Coaches.t as Tie,
         postg as Playoff_Games,postw as Playoff_Win,postl as Playoff_Loose,postt as Playoff_Tie
-        FROM Coaches JOIN Teams ON (Coaches.tmID=Teams.tmID) WHERE (coachID="{coachID}") GROUP BY Coaches.year,Teams.name""")
+        FROM Coaches JOIN Teams ON (Coaches.tmID=Teams.tmID) WHERE (coachID=?) GROUP BY Coaches.year,Teams.name""", (coachID,))
         stats = cursor.fetchall()
         cursor.execute(f"""SELECT AwardsCoaches.year as Season,award as Award
-        FROM Coaches JOIN AwardsCoaches ON (AwardsCoaches.year=Coaches.year)AND(AwardsCoaches.coachID=Coaches.coachID) WHERE (Coaches.coachID="{coachID}")""")
+        FROM Coaches JOIN AwardsCoaches ON (AwardsCoaches.year=Coaches.year)AND(AwardsCoaches.coachID=Coaches.coachID) WHERE (Coaches.coachID=?)""", (coachID,))
         awards = cursor.fetchall()
     
     return render_template('coach.html',general_headers=["firstName","lastName","nameNick","height","weight","firstNHL","lastNHL","Position","birthYear","birthMon",
--- project/blueprints/player/__init__.py
+++ project/blueprints/player/__init__.py
@@ -11,14 +11,14 @@ def index(playerID):
         cursor = connection.cursor()
         cursor.execute(f"""SELECT firstName,lastName,nameNick,height,weight,firstNHL,lastNHL,pos as Position,birthYear,
         birthMon,birthDay,birthCountry,birthCity,deathYear,deathMon,deathDay,deathCountry,deathCity
-        FROM Master WHERE (playerID="{playerID}")""")
+        FROM Master WHERE (playerID=?)""", (playerID,))
         general = cursor.fetchall()
         cursor.execute(f"""SELECT Scoring.year as Season,Scoring.lgID as League,Teams.name as Team,pos as Position,GP as Games_Played,
         Scoring.G as Goals,A as Assists,Scoring.Pts as Points,PIM,"+/-",PPG,PPA,SHG,SHA,GWG,GTG,SOG
-        FROM Scoring JOIN Teams ON (Scoring.tmID=Teams.tmID) WHERE (playerID="{playerID}") GROUP BY Scoring.year,Teams.name""")
+        FROM Scoring JOIN Teams ON (Scoring.tmID=Teams.tmID) WHERE (playerID=?) GROUP BY Scoring.year,Teams.name""", (playerID,))
         stats = cursor.fetchall()
         cursor.execute(f"""SELECT AwardsPlayers.year as Season,award as Award
-        FROM Scoring JOIN AwardsPlayers ON (AwardsPlayers.year=Scoring.year)AND(AwardsPlayers.playerID=Scoring.playerID) WHERE (Scoring.playerID="{playerID}")""")
+        FROM Scoring JOIN AwardsPlayers ON (AwardsPlayers.year=Scoring.year)AND(AwardsPlayers.playerID=Scoring.playerID) WHERE (Scoring.playerID=?)""", (playerID,))
         awards = cursor.fetchall()
     
     return render_template('player.html',general_headers=["firstName","lastName","nameNick","height","weight","firstNHL","lastNHL","Position","birthYear","birthMon",
